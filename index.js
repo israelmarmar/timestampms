2c694d9b80a70fcac8c9bb8a4cecfcf4ea0fd7c2
israel marques martins <emaill.israel.br@gmail.com>
2017-07-28 20:57:13 -0400

    adding files

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3000
+
+ var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+function tm(unix_tm) {
+       var a = new Date(unix_tm * 1000);
+ 
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = month + ' '+date+', '+year;
+  return time;
+
+    }
+
+function dt(date){
+var y=date.split(", ");
+var m=months.indexOf(y[0].split(" ")[0])+1;
+var d=y[0].split(" ")[1];
+y=y[1];
+
+return new Date(m+"/"+d+"/"+y).getTime()/1000;
+
+}
+
+function isInteger(n) { 
+      return !isNaN(parseInt(n))
+}
+
+app.get('/:time', function (req, res) {
+
+var time=req.params.time;
+
+if (isInteger(time))
+res.json({ unix: parseInt(time), natural: tm(time)});
+else
+res.json({ unix: dt(time), natural: time});
+
+})
+app.listen(port, function () {
+ console.log(Date.now());
+})
