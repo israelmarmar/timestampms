549608394e327cc7f5d7622ea7e2b6b37f54c131
Israel Marques Martins <email.israel.br@gmail.com>
2017-07-29 13:24:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -36,8 +36,10 @@ app.get('/:time', function (req, res) {
 
 var time=req.params.time;
  
- if (!(time))
+ if (!(time)){
 res.json({ unix: null, natural: null);
+  return;
+   }
 
 if (isInteger(time))
 res.json({ unix: parseInt(time), natural: tm(time)});
